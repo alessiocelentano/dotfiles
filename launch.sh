404ce40a2872ee3eb4aede03b1ea60dbd375d6f8
alessiocelentano <alessiocelentano2003@gmail.com>
2020-04-13 17:08:11 +0200

    Update Dotfiles

--- launch.sh
+++ launch.sh
@@ -6,5 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# Launch bar
+polybar -c ~/.config/polybar/config.ini media &
+polybar -c ~/.config/polybar/config.ini control &
+polybar -c ~/.config/polybar/config.ini date &
